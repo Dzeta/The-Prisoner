11012013d0ff7f8f262e3e4bdfff70052c12ff51
snwfog <donchoa@gmail.com>
2013-11-27 19:06:45 -0500

    Start rewrite base on new generic2Dsprite and generic2danimatedsprite

--- Cold_Ship/Generic/Generic2DSprite.cs
+++ Cold_Ship/Generic/Generic2DSprite.cs
@@ -10,24 +10,48 @@ namespace Cold_Ship
     // Generic Sprite 2D class for inheritance
     public class GenericSprite2D
     {
+      public GameLevel CurrentGameLevel; // A refernece to this object/sprite exists in which level
         public Texture2D Texture;
         public Vector2 Position;
         public Rectangle BoundBox;
 
-        public GenericSprite2D(Texture2D texture) : this(texture, Vector2.One, Rectangle.Empty) { }
-        public GenericSprite2D(Texture2D texture, Vector2 position) : this(texture, position, Rectangle.Empty) { }
-
-        public GenericSprite2D(Texture2D texture, Vector2 position, Rectangle boundBox)
+        protected GenericSprite2D(GameLevel instance, Texture2D texture) 
+            : this(instance, texture, Vector2.One, Rectangle.Empty) { }
+        public GenericSprite2D(GameLevel instance, Texture2D texture, Vector2 position) 
+            : this(instance, texture, position, Rectangle.Empty) { }
+        protected GenericSprite2D(GameLevel instance, Texture2D texture
+            , Vector2 position, Rectangle boundBox)
         {
             this.Texture = texture;
             this.Position = position;
             this.BoundBox = boundBox;
+          this.CurrentGameLevel = instance;
         }
+
+      public Vector2 GetSize()
+      {
+        Vector2 _size = new Vector2(this.BoundBox.Width, this.BoundBox.Height);
+
+        return _size;
+      }
+
+      public Rectangle GetDefaultBoundingBox()
+      {
+        Rectangle _box = new Rectangle((int)this.Position.X, (int)this.Position.Y, this.Texture.Width, this.Texture.Height);
+        return _box;
+      }
+
+      public Rectangle GetBoundingBox()
+      {
+        return this.BoundBox;
+      }
 
         public bool CheckCollision(GenericSprite2D sprite)
         {
             return this.BoundBox.Intersects(sprite.BoundBox);
         }
+
+      public virtual void Update(GameTime gameTime) { }
 
         public virtual void Draw(SpriteBatch spriteBatch, Vector2 drawPosition)
         {
---
+++ Cold_Ship/Generic/GenericAnimated2DSprite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Cold_Ship.Generic
+{
+  public class GenericAnimated2DSprite : GenericSprite2D
+  {
+    public static Queue<Texture2D> ANIMATION_TEXTURES_FOR_LOADING;
+
+    protected Queue<Texture2D> Textures; 
+    protected int _FrameSwitchTime = 200; // Time between drawing
+    protected int _FrameTimer;
+
+    public GenericAnimated2DSprite(GameLevel instance, Queue<Texture2D> textures, Vector2 position)
+      : base(instance, textures.Peek(), position)
+    {
+      this.Textures = textures;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+      _FrameTimer += gameTime.ElapsedGameTime.Milliseconds;
+      if (_FrameTimer >= _FrameSwitchTime)
+      {
+        _FrameTimer = 0;
+        this.Texture = Textures.Dequeue();
+        this.Textures.Enqueue(this.Texture);
+      }
+    }
+
+    // CAN BE OVERWRITE IF NEEDED, SO FAR THEY DO NOTHING
+    public override void Draw(SpriteBatch spriteBatch
+      , Vector2 drawPosition)
+    {
+      base.Draw(spriteBatch, drawPosition);
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+      base.Draw(spriteBatch);
+    }
+  }
+}
